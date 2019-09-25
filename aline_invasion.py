a93530f872e68c6bb1bcbd0b23625d51fc770c6e
wangtt <442091019@qq.com>
2019-09-25 17:46:43 +0800

    2.设置背景颜色以及创建设置类

--- aline_invasion.py
+++ aline_invasion.py
@@ -6,12 +6,11 @@ import pygame
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    # screen_width = 600
-    # screen_height = 800
-    # screen = pygame.display.set_mode((screen_width, screen_height))
-    screen = pygame.display.set_mode((1200, 800))
-
+    screen = pygame.display.set_mode((600, 800))
     pygame.display.set_caption("Aline Invasion")
+
+    # 设置背景色
+    bg_color = (230, 230, 230)
 
     # 开始游戏的主循环
     while True:
@@ -20,8 +19,17 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环时都重绘屏幕
+        screen.fill(bg_color)
+
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
+
+
+
+
 
 
 run_game()
