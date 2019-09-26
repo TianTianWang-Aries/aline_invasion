9d9e7b0fee6da721964c5fa8af962b8b7c5e36df
wangtt <442091019@qq.com>
2019-09-26 14:09:08 +0800

    重构模块game_function()

---
+++ game_functions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Tami'
+__date__ = '2019-09-26 11:39'
+
+import sys
+import pygame
+
+
+def check_events():
+    """响应鼠标和按键事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(ai_settings, screen, ship, ):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
+
